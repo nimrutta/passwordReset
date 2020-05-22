c352d22cec2ee021c4a2b712efc04ec292156b6b
nimrutta <nimrutta@gmail.com>
2020-05-22 10:44:30 +0300

    Initial setup

---
+++ src/lib/passwordReset.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import css from './style.css';
+
+function PasswordReset({title = 'Reset Your Password'}){
+
+        return (
+          <div className= 'form-background'>
+              <form action="" className= 'form'>
+                <h3>{title}</h3>
+                <label htmlFor="old-password">Old Password</label>
+                <input type="password"/>
+
+                <label htmlFor="new-password">New Password</label>
+                <input type="password"/>
+
+                <label htmlFor="confirm-password">Confirm Password</label>
+                <input type="password"/>
+
+              </form>
+          </div>
+        );
+
+}
+
+export default PasswordReset;
