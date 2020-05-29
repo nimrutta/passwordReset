bdd7e2b4e1cc353e2e6bb47e4edd26b7bd1d76a4
nimrutta <nimrutta@gmail.com>
2020-05-29 10:38:21 +0300

    Improve form UI

--- src/lib/passwordReset.jsx
+++ src/lib/passwordReset.jsx
@@ -7,14 +7,20 @@ function PasswordReset({title = 'Reset Your Password'}){
           <div className= 'form-background'>
               <form action="" className= 'form'>
                 <h3>{title}</h3>
-                <label htmlFor="old-password">Old Password</label>
-                <input type="password"/> <br/>
+                <div>
+                  <label htmlFor="old-password">Old Password</label>
+                  <input type="password"/> <br/>
+                </div>
 
-                <label htmlFor="new-password">New Password</label>
-                <input type="password"/> <br/>
+                <div>
+                  <label htmlFor="new-password">New Password</label>
+                  <input type="password"/> <br/>
+                </div>
 
-                <label htmlFor="confirm-password">Confirm Password</label>
-                <input type="password"/>
+                <div>
+                  <label htmlFor="confirm-password">Confirm Password</label>
+                  <input type="password"/>
+                </div>
 
               </form>
           </div>
